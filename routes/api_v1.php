fea4127a06cad65b040e9d25d6b0cb119764717c
otavio-araujo <otavio_araujo@hotmail.com>
2024-08-10 01:01:58 -0300

    How to Version Your API.

---
+++ routes/api_v1.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Api\V1\TicketController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('tickets', TicketController::class);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
