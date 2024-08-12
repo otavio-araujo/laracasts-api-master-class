d239e73c4115696f5e4f1689bef87b7984093637
otavio-araujo <otavio_araujo@hotmail.com>
2024-08-12 18:05:37 -0300

    Sorting Data.

--- app/Http/Filters/V1/QueryFilter.php
+++ app/Http/Filters/V1/QueryFilter.php
@@ -9,6 +9,7 @@ abstract class QueryFilter
 {
     protected $builder;
     protected $request;
+    protected $sortable = [];
 
     public function __construct(Request $request)
     {
@@ -24,6 +25,32 @@ abstract class QueryFilter
         }
 
         return $this->builder;
+    }
+
+    protected function sort($array)
+    {
+        $sortAttributes = explode(',', $array);
+
+        foreach ($sortAttributes as $sortAttribute) {
+            $direction = 'asc';
+
+            if (strpos($sortAttribute, '-') === 0) {
+                $direction = 'desc';
+                $sortAttribute = substr($sortAttribute, 1);
+            }
+
+            if (!in_array($sortAttribute, $this->sortable) && !array_key_exists($sortAttribute, $this->sortable)) {
+                continue;
+            }
+
+            $columnName = $this->sortable[$sortAttribute] ?? null;
+
+            if ($columnName === null) {
+                $columnName = $sortAttribute;
+            }
+
+            $this->builder->orderBy($columnName, $direction);
+        }
     }
 
     public function apply(Builder $builder)
